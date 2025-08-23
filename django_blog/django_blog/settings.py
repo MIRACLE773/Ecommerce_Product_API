af5a31e92ddcd1a28dace8443b03a87f506cc230
MIRACLE773 <mjschool48@gmail.com>
2025-08-23 09:14:26 -0700

    blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'golden_heritage',
+        'USER': "root",
+        'PASSWORD': 'MJMUSIC',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
