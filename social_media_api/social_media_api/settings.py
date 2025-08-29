2ff7b2b0dab472ff08ac879e44c4b32f67308043
miracle joseph <mjschool48@gmail.com>
2025-08-29 09:26:27 -0700

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "miraclejoseph.pythonanywhere.com", "social-media-api-2bqi.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "miraclejoseph.pythonanywhere.com", "social-media-api-2bqi.onrender.com", "0.0.0.0"]
 
 
 # Application definition
