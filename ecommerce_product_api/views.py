30abd134e118a8474b5ada36901c8a2ce4d031e4
MIRACLE773 <mjschool48@gmail.com>
2025-08-31 12:58:54 -0700

    Your commit message here

--- ecommerce_product_api/views.py
+++ ecommerce_product_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from products.models import Product   # import your model
+from products.models import Product
 
 def home(request):
-    products = Product.objects.all()   # fetch all products
+    products = Product.objects.all()  # fetch all products
     return render(request, 'home.html', {'products': products})
+
