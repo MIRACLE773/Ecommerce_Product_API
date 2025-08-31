7f84164c88b77670734650bb2606332b9bee762f
MIRACLE773 <mjschool48@gmail.com>
2025-08-31 07:26:00 -0700

    Initial commit of ecommerce project

--- django_blog/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, Comment, Profile  # add Book if you have a Book model
-
-# Register models to show in Django admin
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Profile)
-# admin.site.register(Book)  # uncomment this if you have a Book model
--- social_media_api/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserView, FollowUserView, UnfollowUserView
-
-urlpatterns = [
-    path("me/", UserView.as_view(), name="user-detail"),
-    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user"),
-    path("unfollow/<int:user_id>/", UnfollowUserView.as_view(), name="unfollow-user"),
-]
