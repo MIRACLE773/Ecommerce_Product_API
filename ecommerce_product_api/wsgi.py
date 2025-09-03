c5f7a85b9b421e7625dd3d6ac63a82f5878789f3
MIRACLE773 <mjschool48@gmail.com>
2025-09-02 20:51:34 -0700

    deployment on render

--- ecommerce_product_api/wsgi.py
+++ ecommerce_product_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_product_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_product_API.settings')
 
 application = get_wsgi_application()
