80ead4a02d125ef16993cb1c7c03cf9239f95187
miracle joseph <mjschool48@gmail.com>
2025-09-05 16:24:29 -0700

    Delete orders directory

--- orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import OrderListView, OrderDetailView, CreateOrderView
-
-urlpatterns = [
-    path('', OrderListView.as_view(), name='order-list'),
-    path('create/', CreateOrderView.as_view(), name='order-create'),
-    path('<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
-]
