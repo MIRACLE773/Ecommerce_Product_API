dbf54da7a48b35f2d41fe90d1497130156f7b1b7
miracle joseph <mjschool48@gmail.com>
2025-09-05 16:22:25 -0700

    Delete cart directory

--- cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CartRetrieveView, AddCartItemView, UpdateCartItemView, RemoveCartItemView
-
-urlpatterns = [
-    path('', CartRetrieveView.as_view(), name='cart-detail'),
-    path('add/', AddCartItemView.as_view(), name='cart-add'),
-    path('item/<int:item_id>/update/', UpdateCartItemView.as_view(), name='cart-item-update'),
-    path('item/<int:item_id>/remove/', RemoveCartItemView.as_view(), name='cart-item-remove'),
-]
