10a79e8bb941725751076aaef6909aad69f4afac
miracle joseph <mjschool48@gmail.com>
2025-09-05 16:24:41 -0700

    Delete products directory

--- products/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Product, Category
-
-# Register models to appear in the admin site
-admin.site.register(Category)
-admin.site.register(Product)
--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('', ProductListCreateView.as_view(), name='product-list'),
-    path('<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
-]
